8092e4a766f627c91ddc9e5d60060580e14c0caf
MSamir9 <75063256+MSamir9@users.noreply.github.com>
2021-01-11 17:28:48 +0100

    Create model

---
+++ app.py
@@ -0,0 +1,38 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from data_cleaning import clean_text
+from textblob import TextBlob
+from flask_ngrok import run_with_ngrok
+
+
+app = Flask(__name__, template_folder='/content/templates')
+run_with_ngrok(app)
+
+model = pickle.load(open('LR_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    input_text = [str(value) for value in request.form.values()][0]
+    blob = TextBlob(input_text)
+    trans=blob.translate(from_lang='ar', to='en')
+    cleaned_text = [clean_text (str(trans))]
+    
+    prediction = model.predict_proba(cleaned_text)[0]
+    pred_scores = [round(score * 100, 2) for score in prediction]
+    results = dict(zip(model.classes_, pred_scores))
+    sorted_results = [(k, v) for k, v in sorted(results.items(), key=lambda item: item[1], reverse=True)]
+    top_result = sorted_results[0]
+
+    return render_template('index.html', prediction=f'The topic is {top_result[1]}%  {top_result[0]}.')
+
+
+if __name__ == "__main__":
+    app.run()
